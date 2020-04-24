e13c7d32e7c224ee1987f198b2e15618463c2c25
王志敏 <wangzhmt@yonyou.com>
2020-04-24 11:18:43 +0800

    修改可观察的数据

---
+++ src/index4.js
@@ -0,0 +1,63 @@
+import { observable, computed,autorun,when,reaction,action,runInAction } from "mobx";
+
+class Store {
+  @observable array = [];
+  @observable obj = {};
+  @observable map = new Map();
+  @observable string = "helloworld";
+  @observable string1 = "helloworld";
+  @observable number = 1234;
+  @observable number1 = 1234;
+  @observable bool = false;
+  @computed get mixed(){
+      console.log("函数内 computed的mixed")
+    return this.string + "/" + this.number+this.bool;
+  }
+  @action bar(){
+      this.string="@action修改后字符串"
+      this.number="@action修改后数字"
+  }
+  @action.bound barbound(){
+    this.string="action.bound修改后字符串"
+    this.number="action.bound修改后数字"
+}
+}
+
+var store = new Store();
+// var foo = computed(() => {
+//   return store.string + "/" + store.number;
+// });
+// foo.observe((change) => {//每次修改的时候触发监听此函数下修改的属性值
+//     console.log("computed方法监听string,number修改",change)
+// });
+// store.string="123456789"
+// store.number=987654321
+
+// autorun(()=>{//自动触发一次，之后没修改一次触发一次，监听此函数下修改的属性值
+//     console.log("autorun",`数值${store.number1}`,`字符串${store.string1}`)
+// })
+// autorun(()=>{//此方法是数值全修改完触发
+//     console.log("监听computed的autorun",store.mixed)
+// })
+//  store.string1="修改后字符串"
+//  store.number1="修改后数字"
+// when(()=>store.bool,()=>{
+//     console.log("when函数是第一个值为真时,自动触发when函数")
+// })
+// store.bool=true
+// reaction(()=>[store.string,store.number],arr=>{
+//     console.log("reaction监听,数组里边的值没修改一次，触发一次")
+// })
+// store.string="1111111111111111111111";
+// store.number=222222222222222222222
+reaction(()=>[store.string,store.number],arr=>{
+    console.log(`action监听,store.string:${store.string},store.number:${store.number}数组里边的值没修改一次,触发一次`)
+})
+// store.bar()
+// var barbound=store.barbound;
+// barbound()
+
+runInAction(()=>{
+  store.string="1111111111111111111111";
+ store.number=222222222222222222222
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 const config =  {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src/index3.js'),
+    entry: path.resolve(__dirname, 'src/index4.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
